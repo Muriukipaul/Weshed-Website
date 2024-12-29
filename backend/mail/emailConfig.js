1129b922f061c9f797f2f9cd91e5ae8f439f0dfe
Muriukipaul <158034145+Muriukipaul@users.noreply.github.com>
2024-12-29 22:47:14 +0300

    Create emailConfig.js

---
+++ backend/mail/emailConfig.js
@@ -0,0 +1,15 @@
+import nodemailer from 'nodemailer';
+import "../config.js";
+
+export const transporter = nodemailer.createTransport({
+    host: "smtp.gmail.com",
+  port: 587,
+  secure: false, // true for port 465, false for other ports
+  auth: {
+    user: global.email,
+    pass: "global.pass_key",
+  },
+});
+
+
+  
