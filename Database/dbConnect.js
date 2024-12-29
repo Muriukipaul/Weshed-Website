cddf259b62fd7df2e22eaea10d54c9422e2412d2
Muriukipaul <158034145+Muriukipaul@users.noreply.github.com>
2024-12-29 19:41:56 +0300

    Update dbConnect.js

--- Database/dbConnect.js
+++ Database/dbConnect.js
@@ -1,1 +1,14 @@
 
+import mongoose from "mongoose";
+import "../config.js"
+
+export const connectDB = async () => {
+	try {
+		console.log("mongo_uri: ", global.db);
+		const conn = await mongoose.connect(global.db);
+		console.log(`MongoDB Connected: ${conn.connection.host}`);
+	} catch (error) {
+		console.log("Error connection to MongoDB: ", error.message);
+		process.exit(1);
+	}
+};
